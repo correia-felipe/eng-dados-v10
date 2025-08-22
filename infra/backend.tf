b453ca7d004ddac64ff6116879cc82eacc81f3dd
Felipe Correia <correia.felipe@hotmail.com>
2025-08-22 20:24:36 -0300

    fix

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "artifactory-847623453769"
-    key            = "backend-tf/infra-governance/terraform.tfstate"
+    key            = "backend-tf/eng-dados-v10/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
   }
